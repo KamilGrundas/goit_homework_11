84547d6ab2a4a824278187ec3b63aa3b08b20855
Kamil Grundas <kamil.grundas@gmail.com>
2024-02-26 13:10:23 +0100

    Added birthday test and search by string test

--- tests/test_route_contacts.py
+++ tests/test_route_contacts.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import MagicMock, patch
 from src.services.auth import auth_service
+from datetime import datetime, date, timedelta
 
 
 def test_create_contact(client, token):
@@ -119,3 +120,82 @@ def test_repeat_delete_contact(client, token):
         assert response.status_code == 404, response.text
         data = response.json()
         assert data["detail"] == "Contact not found"
+
+def test_repeat_delete_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.delete(
+            "/api/contacts/1",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 404, response.text
+        data = response.json()
+        assert data["detail"] == "Contact not found"
+
+def test_repeat_delete_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.delete(
+            "/api/contacts/1",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 404, response.text
+        data = response.json()
+        assert data["detail"] == "Contact not found"
+
+def test_get_contact_got_birthday(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        today = date.today()
+        client.post(
+            "/api/contacts",
+            json=  {
+                    "forename": "Kamil",
+                    "surname": "Cayld",
+                    "email": "kamil.clayd@gmail.com",
+                    "phone_number": "+48605412281",
+                    "born_date": str(today + timedelta(days=2)),
+                    },
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        client.post(
+            "/api/contacts",
+            json=  {
+                    "forename": "Tadeusz",
+                    "surname": "Romanowski",
+                    "email": "tadek@example.com",
+                    "phone_number": "+48601123421",
+                    "born_date": str(today + timedelta(days=5)),
+                    },
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        client.post(
+            "/api/contacts",
+            json=  {
+                    "forename": "Patrycja",
+                    "surname": "Bolgusz",
+                    "email": "bolgusz_patrycja@example.com",
+                    "phone_number": "+48664125551",
+                    "born_date": str(today + timedelta(days=10)),
+                    },
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        response = client.get(
+            "/api/contacts/birthdays/",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert len(data) == 2
+
+def test_get_contact_by_string(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.get(
+            "/api/contacts/search/?search_by=Pat",
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert len(data) == 1
+        assert data[0]["email"] == "bolgusz_patrycja@example.com"
